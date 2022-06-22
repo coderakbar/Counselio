a4731bac6543cae06fdeafb09a7dde609fac1c59
coderakbar <98220168+coderakbar@users.noreply.github.com>
2022-06-22 12:55:19 +0530

    some more changes on drop down menus

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,1 +1,8 @@
- 
+function dropdown() {  
+    var click = document.getElementById("dropdown1");  
+    if(click.style.display ==="none") {  
+       click.style.display ="block";  
+    } else {  
+       click.style.display ="none";  
+    }   
+ }  
